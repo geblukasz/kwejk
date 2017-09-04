1c09e99a783e811ad20448f529180420c6989339
Lukasz Gebicki <geblukasz@gmail.com>
2017-09-04 16:48:32 +0200

    added a static block

--- src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
@@ -1,13 +1,26 @@
 package pl.akademiakodu.kwejk.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.akademiakodu.kwejk.dao.GifDao;
 
 @Controller
 public class GifController {
 
+    @Autowired
+    private GifDao gifDao;
+
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/gif/{title}")
+    public String details(@PathVariable String title, ModelMap modelMap){
+        modelMap.addAttribute("gif", gifDao.findOne(title));
+        return "gif-details";
     }
 }
--- src/main/java/pl/akademiakodu/kwejk/dao/GifDaoImpl.java
+++ src/main/java/pl/akademiakodu/kwejk/dao/GifDaoImpl.java
@@ -1,21 +1,47 @@
 package pl.akademiakodu.kwejk.dao;
 
+import org.springframework.stereotype.Component;
 import pl.akademiakodu.kwejk.model.Gif;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class GifDaoImpl implements GifDao {
 
-    Gif gif = new Gif();
+    public GifDaoImpl(){}
+
+    private static List<String> titles = new ArrayList<>();
+
+    public static List<String> getTitles() {
+        return titles;
+    }
+
+    public static void setTitles(List<String> titles) {
+        GifDaoImpl.titles = titles;
+    }
+
+    static{
+        titles.add("android-explosion");
+        titles.add("ben-and-mike");
+        titles.add("book-dominos");
+        titles.add("compiler-bot");
+        titles.add("cowboy-coder");
+        titles.add("infinite-andrew");
+    }
     @Override
     public List<Gif> findAll() {
-        return null;
+        List<Gif> gifs = new ArrayList<>();
+        int i = 1;
+        for(String title : titles){
+            gifs.add(new Gif(title, "username"+i++));
+        }
+        return gifs;
     }
 
     @Override
     public Gif findOne(String title){
         return null;
-        //.stream().filter((p)->p.getSurname().equals(surname)).collect(Collectors.toList());
     }
 }
